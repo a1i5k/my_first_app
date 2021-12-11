06927925ac115e6a6476fa6a1ab7faf8a98a7e05
kolyansp22222@mail.ru <kolyansp22222@mail.ru>
2021-12-11 12:28:13 +0300

    change host

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def get_money_handler():
     return json.dumps({'user_id': user_id, 'money': user.get(user_id)['money']})
 
 if __name__=='__main__':
-    app.run(host='127.0.0.1', port=5001)
+    app.run(host='0.0.0.0', port=5001)
